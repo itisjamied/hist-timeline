2491b5005e9ec4a04eeba07f20907359b41fd49b
Jamie <77027304+itisjamied@users.noreply.github.com>
2025-05-03 16:27:27 -0400

    renamed some stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import { sanityClient } from '@/sanity/lib/client'
+import { client } from '@/sanity/lib/client'
 
 export default async function Home() {
   // GROQ to grab just the title field
-  const settings = await sanityClient.fetch<{title: string}>(
+  const settings = await client.fetch<{title: string}>(
     `*[_type == "siteSettings"][0]{ title }`
   )
 
